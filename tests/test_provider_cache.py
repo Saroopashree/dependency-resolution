fbbe19fb91dda1be487fe621640b5db8d73aa51d
Saroopashree Kumaraguru <saroopa25@gmail.com>
2023-02-13 21:48:08 +0530

    Move ProviderCache from __init__ to separate file. Add tests

---
+++ tests/test_provider_cache.py
@@ -0,0 +1,78 @@
+from unittest import TestCase
+
+from dependency_resolution import ProviderCache
+
+
+class Image:
+    def __init__(self, file: str) -> None:
+        self.file = file
+
+
+class RGBImage(Image):
+    ...
+
+
+class TestProviderCache(TestCase):
+    def test_add_dependency_by_addition_assignment(self):
+        def func1():
+            cache = ProviderCache.get_instance()
+            cache += Image("image.png")
+
+        func1()
+
+        cache = ProviderCache.get_instance()
+
+        assert cache.get_instance() is cache
+        assert type(cache[Image]) == Image
+        assert cache[Image].file == "image.png"
+
+    def test_add_dependency_by_setitem(self):
+        def func1():
+            cache = ProviderCache.get_instance()
+            cache[Image] = Image("image.png")
+
+        func1()
+
+        cache = ProviderCache.get_instance()
+
+        assert type(cache[Image]) == Image
+        assert cache[Image].file == "image.png"
+
+    def test_add_dependency_by_setitem_with_wrong_type(self):
+        def func1():
+            cache = ProviderCache.get_instance()
+            cache[Image] = 1
+
+        with self.assertRaises(ValueError) as e:
+            func1()
+
+        assert e.exception.args[0] == "Provided object is not of type Image (<class 'tests.test_provider_cache.Image'>)"
+
+    def test_add_dependency_under_parent_class(self):
+        def func1():
+            cache = ProviderCache.get_instance()
+            cache[Image] = RGBImage("image.png")
+
+        func1()
+
+        cache = ProviderCache.get_instance()
+        assert type(cache[Image]) == RGBImage
+        assert cache[Image].file == "image.png"
+
+    def test_overwrite_added_dependency(self):
+        def func1():
+            cache = ProviderCache.get_instance()
+            cache[Image] = Image("image.png")
+
+        func1()
+
+        def func2():
+            cache = ProviderCache.get_instance()
+            cache[Image] = Image("image2.png")
+
+        func2()
+
+        cache = ProviderCache.get_instance()
+
+        assert type(cache[Image]) == Image
+        assert cache[Image].file == "image2.png"
