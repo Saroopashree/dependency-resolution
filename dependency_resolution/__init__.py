30d896112afd80c41962e84c445f068f15a88e26
Saroopashree Kumaraguru <saroopa25@gmail.com>
2023-02-13 19:50:32 +0530

    Initial code commit

---
+++ dependency_resolution/__init__.py
@@ -0,0 +1,27 @@
+from typing import Optional, Type, TypeVar
+
+
+TItem = TypeVar("TItem")
+
+
+class CacheContainer:
+    __instance: Optional["CacheContainer"] = None
+    __objects: dict[Type[TItem], TItem] = {}
+
+    @classmethod
+    def get_instance(cls) -> "CacheContainer":
+        if cls.__instance is None:
+            cls.__instance = cls()
+        return cls.__instance
+
+    def __iadd__(self, other: TItem) -> "CacheContainer":
+        self.__objects[other.__class__] = other
+        return self
+
+    def __getitem__(self, type: Type[TItem]) -> TItem:
+        return self.__objects[type]
+
+    def __setitem__(self, type: Type[TItem], object: TItem) -> None:
+        if type not in object.__class__.__mro__:
+            raise ValueError(f"Provided object is not of type {type}")
+        self.__objects[type] = object
