fb321016195e924e2cda66ece3eac565185c2e6d
Saroopashree Kumaraguru <saroopa25@gmail.com>
2023-02-20 17:26:20 +0530

    fix: expose MockProvider in the root

--- dependency_resolution/__init__.py
+++ dependency_resolution/__init__.py
@@ -1,4 +1,5 @@
 from dependency_resolution.auto_wired_cache import AutoWiredCache
 from dependency_resolution.provider_cache import ProviderCache
+from dependency_resolution.mocks import MockProvider
 
-__all__ = ["AutoWiredCache", "ProviderCache"]
+__all__ = ["AutoWiredCache", "ProviderCache", "MockProvider"]
